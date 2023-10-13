a0932d4907b22624b98e1f0353a10684f63a5153
forest <adityaforestresananta@gmail.com>
2023-10-14 00:48:50 +0700

    construct ui landing page

---
+++ frontend/src/pages/faq/index.vue
@@ -0,0 +1,8 @@
+<template>
+    faq
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'landing-page'
+})
+</script>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,10 +1,48 @@
 <template>
-    <section class="flex flex-col w-full h-screen justify-center items-center">
-      <h1>HomePage</h1>
-    </section>
+  <div class="flex flex-col overflow-x-hidden">
+    <div class="font-bold w-full h-fit">
+      <h1 class="absolute m-auto top-[50vh] w-full text-[40px] bg-white text-blue-500 flex justify-center">WE ARE THE LEADER IN CONSTRUCTION INDUSTRY</h1>
+      <img src="@/assets/images/construction-2.jpg" alt="" class="object-cover h-[100vh] w-full">  
+    </div>
+    <div class="flex flex-col px-12">
+      <div class="flex gap-8 flex items-center mt-12 py-12">
+        <p class="text-sm">
+          Our company executes building and infrastructure works. We implement a futuristic
+          style inspired by modern design and art. Our team is ready to construct anything
+          that you depict in the drawing. We never stop constructing until our client is fully satisfied.        
+        </p>
+        <img src="@/assets/images/construction.jpg" alt="" width="500" class="rounded-lg shadow-2xl">        
+      </div>
+      <div class="flex gap-8 flex items-center mt-12 py-12 bg-gray-200 p-12 rounded-lg">
+        <img src="@/assets/images/construction-3.jpg" alt="" width="500" class="rounded-lg shadow-2xl">        
+        <p class="text-sm">
+          Our company executes building and infrastructure works. We implement a futuristic
+          style inspired by modern design and art. Our team is ready to construct anything
+          that you depict in the drawing. We never stop constructing until our client is fully satisfied.        
+        </p>
+      </div>
+      <div class="flex gap-8 flex items-center mt-12 py-12">
+        <p class="text-sm">
+          Our company executes building and infrastructure works. We implement a futuristic
+          style inspired by modern design and art. Our team is ready to construct anything
+          that you depict in the drawing. We never stop constructing until our client is fully satisfied.        
+        </p>
+        <img src="@/assets/images/construction-4.jpg" alt="" width="500" class="rounded-lg shadow-2xl">        
+      </div>
+    </div>
+
+    <footer class="bg-gray-200 flex items-center justify-center text-sm p-12">
+      <h1>@ 2023 Copyright COMPANY LLC</h1>
+    </footer>
+  </div>
 </template>
 
-<script lang="ts" setup>
-
+<script setup>
+definePageMeta({
+  layout: 'landing-page'
+})
 </script>
-  
+
+<style>
+
+</style>
